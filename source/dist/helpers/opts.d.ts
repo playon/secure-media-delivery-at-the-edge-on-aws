3cc7d4fd6e409879b1f5e29fa3a8fcf24d43626e
Corneliu Croitoru <cornel.croitoru@gmail.com>
2022-03-22 14:49:40 +0100

    delete all files

--- source/dist/helpers/opts.d.ts
+++
@@ -1,8 +0,0 @@
-import { IConfiguration } from './validators/configuration';
-/**
- * Returns the options to pass to the Prototype Engagement Pack.
- * This function will validate the confioguration read from the CDK context
- * file, and will pass the resulted value to the caller.
- * @throws an exception if the configuration is not valid.
- */
-export declare const getOpts: () => Promise<IConfiguration>;

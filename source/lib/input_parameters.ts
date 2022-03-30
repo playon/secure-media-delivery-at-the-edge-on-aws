0d61476cd2f8d5d5fc51448a259c90e8813fe445
Corneliu Croitoru <ccroito@amazon.com>
2022-03-30 16:05:40 +0200

    update input values config

--- source/lib/input_parameters.ts
+++ source/lib/input_parameters.ts
@@ -18,6 +18,7 @@ import {
   CfnParameter,
 } from "aws-cdk-lib";
 import { Construct } from "constructs";
+import { isContext } from "vm";
 import { IConfiguration } from "../helpers/validators/configuration";
 
 export class GetInputParameters extends Construct {
@@ -27,99 +28,255 @@ export class GetInputParameters extends Construct {
     super(scope, id);
 
     console.log("configuration=" + JSON.stringify(configuration));
+    var returnObject: IConfiguration;
 
-    const returnObject: IConfiguration = {
-      main: {
-        rotate_secrets_frequency: "24h",
-        rotate_secrets_pattern: "00 12 ? * * *",
-      },
-    };
-
-    if (configuration.demo) {
-      const username = new CfnParameter(this, "username", {
+    if (configuration.main?.rotate_secrets_pattern === "P") {
+      const hours = new CfnParameter(this, "A", {
         type: "String",
-        description:
-          "[API][Demo website] Username used to authenticate demo viewer",
+        allowedValues: [
+          "00",
+          "01",
+          "02",
+          "03",
+          "04",
+          "05",
+          "06",
+          "07",
+          "08",
+          "09",
+          "10",
+          "11",
+          "12",
+          "13",
+          "14",
+          "15",
+          "16",
+          "17",
+          "18",
+          "19",
+          "2O",
+          "21",
+          "22",
+          "23",
+        ],
+        description: "[Base configuration] Hours",
       });
 
-      const password = new CfnParameter(this, "password", {
+      const minutes = new CfnParameter(this, "B", {
         type: "String",
-        description:
-          "[API][Demo website] Password used to authenticate demo viewer",
+        allowedValues: [
+          "00",
+          "01",
+          "02",
+          "03",
+          "04",
+          "05",
+          "06",
+          "07",
+          "08",
+          "09",
+          "10",
+          "11",
+          "12",
+          "13",
+          "14",
+          "15",
+          "16",
+          "17",
+          "18",
+          "19",
+          "2O",
+          "21",
+          "22",
+          "23",
+          "24",
+          "25",
+          "26",
+          "27",
+          "28",
+          "29",
+          "30",
+          "31",
+          "32",
+          "33",
+          "34",
+          "35",
+          "36",
+          "37",
+          "38",
+          "39",
+          "40",
+          "41",
+          "42",
+          "43",
+          "44",
+          "45",
+          "46",
+          "47",
+          "48",
+          "49",
+          "50",
+          "51",
+          "52",
+          "53",
+          "54",
+          "55",
+          "56",
+          "57",
+          "58",
+          "59",
+        ],
+        description: "[Base configuration] Hours",
       });
 
-      returnObject.demo = {
-        username: username.valueAsString,
-        password: password.valueAsString,
+      const day_of_week = new CfnParameter(this, "C", {
+        type: "String",
+        allowedValues: ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"],
+        description: "[Base configuration] Day of the week",
+      });
+
+      const week_of_month = new CfnParameter(this, "D", {
+        type: "String",
+        allowedValues: ["1", "2", "3", "4"],
+        description: "[Base configuration] Day of the week",
+      });
+
+      returnObject = {
+        main: {
+          rotate_secrets_frequency: "1m",
+          rotate_secrets_pattern:
+            minutes +
+            " " +
+            hours +
+            " ? * " +
+            day_of_week +
+            "#" +
+            week_of_month +
+            " *",
+        },
+      };
+    } else {
+      returnObject = {
+        main: {
+          rotate_secrets_frequency: "1m",
+          rotate_secrets_pattern: configuration.main?.rotate_secrets_pattern!,
+        },
       };
     }
+
+    if(configuration.demo){
+
+        if (configuration.demo?.username === "U") {
+            const username = new CfnParameter(this, "E", {
+              type: "String",
+              description:
+                "[API][Demo website] Username used to authenticate demo viewer",
+            });
+
+            const password = new CfnParameter(this, "F", {
+              type: "String",
+              description:
+                "[API][Demo website] Password used to authenticate demo viewer",
+            });
+
+            returnObject.demo = {
+              username: username.valueAsString,
+              password: password.valueAsString,
+            };
+          } else {
+            returnObject.demo = {
+              username: configuration.demo?.username!,
+              password: configuration.demo?.password!,
+            };
+          }
+    }
+
     if (configuration.dash) {
-      const dash_hostname = new CfnParameter(this, "dash_hostname", {
-        type: "String",
-        description: "[API][DASH] Hostname used for asset delivery",
-      });
+      if (configuration.dash?.hostname === "H") {
+        const dash_hostname = new CfnParameter(this, "G", {
+          type: "String",
+          description: "[API][DASH] Hostname used for asset delivery",
+        });
 
-      const dash_url_path = new CfnParameter(this, "dash_url_path", {
-        type: "String",
-        description: "[API][DASH] URL path for existing playable asset",
+        const dash_url_path = new CfnParameter(this, "H", {
+          type: "String",
+          description: "[API][DASH] URL path for existing playable asset",
+        });
 
-      });
+        const dash_ttl = new CfnParameter(this, "I", {
+          type: "String",
+          description: "[API][DASH] TTL for the token",
+          allowedValues: ["+30m", "+1h", "+3h", "+6h", "+24h"],
+          default: configuration.dash?.ttl,
+        });
 
-      const dash_ttl = new CfnParameter(this, "dash_ttl", {
-        type: "String",
-        description: "[API][DASH] TTL for the token",
-        allowedValues: ["+30m", "+1h", "+3h", "+6h", "+24h"],
-        default: configuration.dash?.ttl,
-      });
-
-      returnObject.dash = {
-        hostname: dash_hostname.valueAsString,
-        url_path: dash_url_path.valueAsString,
-        ttl: dash_ttl.valueAsString,
-      };
+        returnObject.dash = {
+          hostname: dash_hostname.valueAsString,
+          url_path: dash_url_path.valueAsString,
+          ttl: dash_ttl.valueAsString,
+        };
+      } else {
+        returnObject.dash = {
+          hostname: configuration.dash?.hostname!,
+          url_path: configuration.dash?.url_path!,
+          ttl: configuration.dash?.ttl!,
+        };
+      }
     }
+    console.log("configuration.hls?.hostname=" + configuration.hls?.hostname);
 
     if (configuration.hls) {
+      if (configuration.hls?.hostname === "H") {
+        const hls_hostname = new CfnParameter(this, "J", {
+          type: "String",
+          description: "[API][HLS] Hostname used for asset delivery",
+        });
 
-      const hls_hostname = new CfnParameter(this, "hls_hostname", {
-        type: "String",
-        description: "[API][HLS] Hostname used for asset delivery",
-      });
+        const hls_url_path = new CfnParameter(this, "K", {
+          type: "String",
+          description: "[API][HLS] URL path for existing playable asset",
+        });
 
-      const hls_url_path = new CfnParameter(this, "hls_url_path", {
-        type: "String",
-        description: "[API][HLS] URL path for existing playable asset",
-      });
+        const hls_ttl = new CfnParameter(this, "L", {
+          type: "String",
+          description: "[API][DASH] TTL for the token",
+          allowedValues: ["+30m", "+1h", "+3h", "+6h", "+24h"],
+          default: configuration.hls?.ttl,
+        });
 
-      const hls_ttl = new CfnParameter(this, "hls_ttl", {
-        type: "String",
-        description: "[API][DASH] TTL for the token",
-        allowedValues: ["+30m", "+1h", "+3h", "+6h", "+24h"],
-        default: configuration.hls?.ttl,
-      });
+        returnObject.hls = {
+          hostname: hls_hostname.valueAsString,
+          url_path: hls_url_path.valueAsString,
+          ttl: hls_ttl.valueAsString,
+        };
+      } else {
+        returnObject.hls = {
+          hostname: configuration.hls?.hostname!,
+          url_path: configuration.hls?.url_path!,
+          ttl: configuration.hls?.ttl!,
+        };
+      }
+    }
+    if(configuration.api){
 
-      returnObject.hls = {
-        hostname: hls_hostname.valueAsString,
-        url_path: hls_url_path.valueAsString,
-        ttl: hls_ttl.valueAsString,
-      };
+        if (configuration.api?.language === "A") {
+            const api_language = new CfnParameter(this, "M", {
+              type: "String",
+              description: "[API] Choose the programming language for API code ",
+              allowedValues: ["nodejs", "python"],
+              default: configuration.api.language,
+            });
+
+            returnObject.api = {
+              language: api_language.valueAsString,
+            };
+          } else {
+            returnObject.api = {
+              language: configuration.api?.language!,
+            };
+          }
     }
 
-    if (configuration.api) {
-      console.log(configuration.api.language);
-      const api_language = new CfnParameter(this, "api_language", {
-        type: "String",
-        description: "[API] Choose the programming language for API code ",
-        allowedValues: ["nodejs", "python"],
-        default: configuration.api.language,
-      });
-
-      console.log("api_language 👉 ", api_language.valueAsString);
-
-      returnObject.api = {
-        language: api_language.valueAsString,
-      };
-    }
 
     console.log("returnObject=" + JSON.stringify(returnObject));
     this.customInputParameters = returnObject;

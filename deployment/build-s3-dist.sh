016dd1fb8168f10931b18893ac111fe99cb3954d
Corneliu Croitoru <ccroito@amazon.com>
2022-04-20 15:52:09 +0200

    mineur fixes

--- deployment/build-s3-dist.sh
+++ deployment/build-s3-dist.sh
@@ -79,7 +79,11 @@ stack_name=`grep -o '"stack_name": "[^"]*' solution.context.json | grep -o '[^"]
 # Run 'cdk synth' to generate raw solution outputs
 echo "cd "$source_dir""
 cd "$source_dir"
+
+chmod +x ./install_dependencies.sh && ./install_dependencies.sh
+
 echo "node_modules/aws-cdk/bin/cdk synth -q --output=$staging_dist_dir"
+
 npm run build && node_modules/aws-cdk/bin/cdk synth -q --output=$staging_dist_dir --no-version-reporting
 
 cdk_bucket_name=`grep -o '"bucketName": "[^"]*' $staging_dist_dir/*.assets.json | grep -o '[^"]*$' | head -1 `

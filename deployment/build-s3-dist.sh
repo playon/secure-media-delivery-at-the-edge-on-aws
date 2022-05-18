044b098918ffc0f81561296957af6b78580201a9
Corneliu Croitoru <ccroito@amazon.com>
2022-05-18 18:25:26 +0200

    cleanup build script

--- deployment/build-s3-dist.sh
+++ deployment/build-s3-dist.sh
@@ -83,22 +83,13 @@ cd "$source_dir"
 chmod +x ./install_dependencies.sh && ./install_dependencies.sh
 
 echo "node_modules/aws-cdk/bin/cdk synth -q --output=$staging_dist_dir"
-ls
+
 npm run build && node_modules/aws-cdk/bin/cdk synth -q --output=$staging_dist_dir --no-version-reporting
 
 cdk_bucket_name=`grep -o '"bucketName": "[^"]*' $staging_dist_dir/*.assets.json | grep -o '[^"]*$' | head -1 `
 
-new_bucket_name="""{"Fn::Join": ["-",["$BUILD_OUTPUT_BUCKET",{"Ref": "AWS::Region"}]]}"""
-
-echo $new_bucket_name
-
-
 echo sed -i'' -e "s#$cdk_bucket_name#$BUILD_OUTPUT_BUCKET-\${AWS::Region}#g" $staging_dist_dir/$stack_name.template.json
-
-##{"Fn::Join": ["-",["$BUILD_OUTPUT_BUCKET",{"Ref": "AWS::Region"}]]}
-
 sed -i'' -e "s#$cdk_bucket_name#$BUILD_OUTPUT_BUCKET-\${AWS::Region}#g" $staging_dist_dir/$stack_name.template.json
-#sed -i'' -e "s#$cdk_bucket_name#"{"Fn::Join": ["-",["$BUILD_OUTPUT_BUCKET",{"Ref": "AWS::Region"}]]}"#g" $staging_dist_dir/$stack_name.template.json
 
 i=1
 cd $staging_dist_dir

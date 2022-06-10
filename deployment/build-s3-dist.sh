0a59e3abca8772fc6331543ca2c50520a923e073
Corneliu Croitoru <ccroito@amazon.com>
2022-06-10 11:53:59 +0200

    fix grep

--- deployment/build-s3-dist.sh
+++ deployment/build-s3-dist.sh
@@ -87,11 +87,13 @@ echo "node_modules/aws-cdk/bin/cdk synth -q --output=$staging_dist_dir"
 npm run build && node_modules/aws-cdk/bin/cdk synth -q --output=$staging_dist_dir --no-version-reporting
 
 cdk_bucket_name=`grep -o '"bucketName": "[^"]*' $staging_dist_dir/$stack_name.assets.json | grep -o '[^"]*$' | head -1 `
-cdk_bucket_name_useast1=`grep -o '"bucketName": "[^"]*' $staging_dist_dir/$stack_nameUsEast1Stack.assets.json | grep -o '[^"]*$' | head -1 `
+echo "bucket_name=$bucket_name"
+cdk_bucket_name_useast1=`grep -o '"bucketName": "[^"]*' $staging_dist_dir/${stack_name}UsEast1Stack.assets.json | grep -o '[^"]*$' | head -1 `
 echo "cdk_bucket_name_useast1=$cdk_bucket_name_useast1"
 
 new_bucket_name="{\"Fn::Sub\": \"$BUILD_OUTPUT_BUCKET-\${AWS::Region}\" }"
 sed -i'' -e s"/\"$cdk_bucket_name\"/$new_bucket_name/" $staging_dist_dir/$stack_name.template.json
+
 
 echo sed -i'' -e s"/\"$cdk_bucket_name_useast1\"/$new_bucket_name/" $staging_dist_dir/${stack_name}UsEast1Stack.template.json
 sed -i'' -e s"/\"$cdk_bucket_name_useast1\"/$new_bucket_name/" $staging_dist_dir/${stack_name}UsEast1Stack.template.json

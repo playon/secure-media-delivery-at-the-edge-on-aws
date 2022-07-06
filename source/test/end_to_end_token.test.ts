4948b00d80131dcc3e87a7b95a5a1ee6d6991d1c
Croitoru <ccroito@bcd074351df1.ant.amazon.com>
2022-07-06 17:23:20 +0200

    add test

--- source/test/end_to_end_token.test.ts
+++ source/test/end_to_end_token.test.ts
@@ -95,7 +95,7 @@ describe("Check token generation", () => {
     };
 
     const result = cff1.handler(cffEvent);
-    console.log(result);
+    
     expect(result.method).toBe("GET");
 
   }, 70000);
@@ -180,7 +180,7 @@ describe("Check token generation", () => {
     };
 
     const result = cff1.handler(cffEvent);
-    console.log(result);
+    
     expect(result.method).toBe("GET");
 
   }, 70000);
@@ -264,7 +264,7 @@ describe("Check token generation", () => {
     };
 
     const result = cff1.handler(cffEvent);
-    console.log(result);
+    
     expect(result.method).toBe("GET");
 
   }, 70000);
@@ -347,8 +347,90 @@ describe("Check token generation", () => {
     };
 
     const result = cff1.handler(cffEvent);
-    console.log(result);
+    
     expect(result.method).toBe("GET");
+
+  }, 70000);
+
+  test("Check token - token not valid yet", async () => {
+
+    const myIp = "54.240.197.233";
+    const myReferer =  'https://mycloudfrontdomainname.cloudfront.net';
+    const myUserAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:91.0) Gecko/20100101 Firefox/91.0';
+
+
+    var viewer_attributes = {
+      "ip": myIp,
+      "headers": {
+        'cloudfront-viewer-address': myIp + ':31830',
+        'cloudfront-viewer-country': 'IE',
+        'content-length': '0',
+        'host': 'un25b5wnf5.execute-api.eu-west-1.amazonaws.com',
+        'referer': myReferer,
+        'user-agent': myUserAgent,
+        'via': '1.1 f9e2b62bbab7f16f69e97695da81e608.cloudfront.net (CloudFront)',
+      }
+    };
+
+    var token_policy =
+    {
+      "co": true,
+      "co_fallback": true,
+      "reg": true,
+      "reg_fallback": true,
+      "exc": [
+        "/ads/"
+      ],
+      "exp": "+3h",
+      "headers": [
+        "user-agent"
+      ],
+      "ip": false,
+      "nbf": "1688656681",
+      "paths": [
+        "/out/v1/00c6ff982d404e2f940b48495b243b3c/"
+      ],
+      "session_auto_generate": 12,
+      "ssn": true
+    };
+
+    const cloudfrontDomainName = "https://videoassetcloudfrontdomainname.com";
+    const mediaUrl = "/out/v1/00c6ff982d404e2f940b48495b243b3c/index.m3u";
+    const playbackUrl = await token1.generate(viewer_attributes, `${cloudfrontDomainName}${mediaUrl}`, token_policy);
+    var start = cloudfrontDomainName.length;
+    const myPath = playbackUrl.substring(start);
+
+    playbackUrl.startsWith(cloudfrontDomainName)
+    expect(playbackUrl.startsWith(cloudfrontDomainName)).toBeTruthy();
+    expect(playbackUrl.endsWith(mediaUrl)).toBeTruthy();
+    var cffEvent = {
+      "version": "1.0",
+      "viewer": {
+        "ip": myIp
+      },
+      "request": {
+        "method": "GET",
+        "uri": myPath,
+        "headers": {
+          "host": {
+            "value": "dklf7fsi4gpzd.cloudfront.net"
+          },
+          "user-agent": {
+            "value": myUserAgent
+          },
+          "referer": {
+            "value": myReferer
+          },
+          "origin": {
+            "value": "https://d26xf765ycwwd4.cloudfront.net"
+          }
+        }
+      }
+    };
+
+    const result = cff1.handler(cffEvent);
+    expect(result.statusCode).toBe(401);
+    expect(result.statusDescription).toBe("Unauthorized");
 
   }, 70000);
 

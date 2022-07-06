0cfe684b7788db03b3f7858a8ddecdd91d563d0f
Croitoru <ccroito@bcd074351df1.ant.amazon.com>
2022-07-06 12:49:15 +0200

    add CFF test

--- source/lambda/generate_secret_update_cff/index.js
+++ source/lambda/generate_secret_update_cff/index.js
@@ -264,3 +264,5 @@ function handler(event) {
     }
 
 }
+
+exports.handler = handler;
---
+++ source/test/check_token_cff.test.ts
@@ -0,0 +1,39 @@
+const cff = require("../lambda/generate_secret_update_cff/index.js");
+
+
+
+describe("Check token", () => {
+    
+  test('Empty event', () => {
+     // arrange and act
+     var cffEvent = {
+      "version":"1.0",
+      "viewer":{
+         "ip":"MY_IP"
+      },
+      "request":{
+         "method":"GET",
+         "uri":"/MYSESSIONID.MY_JWT_TOKEN/out/v1/00c6ff982d404e2f940b48495b243b3c/index.m3u8",         
+         "headers":{
+            "host":{
+               "value":"dklf7fsi4gpzd.cloudfront.net"
+            },
+            "user-agent":{
+               "value":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:91.0) Gecko/20100101 Firefox/91.0"
+            },
+            "referer":{
+               "value":"https://d26xf765ycwwd4.cloudfront.net/"
+            },
+            "origin":{
+               "value":"https://d26xf765ycwwd4.cloudfront.net"
+            }
+         }
+      }
+   };
+     var result = cff.handler(cffEvent);
+     var expectedResult = {"statusCode": 401, "statusDescription": "Unauthorized"};
+     expect(result).toStrictEqual(expectedResult);
+  });
+
+});
+

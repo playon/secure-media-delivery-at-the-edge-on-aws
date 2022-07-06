f4540ec9d1dae5a432f9809f032b7d4c6436d9a4
Croitoru <ccroito@bcd074351df1.ant.amazon.com>
2022-07-06 17:05:12 +0200

    add test

--- source/test/end_to_end_token.test.ts
+++ source/test/end_to_end_token.test.ts
@@ -15,9 +15,9 @@ jest.mock("aws-sdk") // jest will automatically find the mock
 
 describe("Check token generation", () => {
 
-  test("Check token - valid token ", async () => {
+  test("Check token - valid token, ip=false ", async () => {
 
-    const myIp = "MY_IP";
+    const myIp = "54.240.197.233";
     const myReferer =  'https://mycloudfrontdomainname.cloudfront.net';
     const myUserAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:91.0) Gecko/20100101 Firefox/91.0';
 
@@ -28,7 +28,7 @@ describe("Check token generation", () => {
       "reg": "ILE DE FRANCE",
       "cty": "PARIS",
       "headers": {
-        'cloudfront-viewer-address': '54.240.197.233:31830',
+        'cloudfront-viewer-address': myIp + ':31830',
         'cloudfront-viewer-country': 'IE',
         'content-length': '0',
         'host': 'un25b5wnf5.execute-api.eu-west-1.amazonaws.com',
@@ -52,6 +52,91 @@ describe("Check token generation", () => {
         "user-agent"
       ],
       "ip": false,
+      "nbf": "1645000000",
+      "paths": [
+        "/out/v1/00c6ff982d404e2f940b48495b243b3c/"
+      ],
+      "session_auto_generate": 12,
+      "ssn": true
+    };
+
+    const cloudfrontDomainName = "https://videoassetcloudfrontdomainname.com";
+    const mediaUrl = "/out/v1/00c6ff982d404e2f940b48495b243b3c/index.m3u";
+    const playbackUrl = await token1.generate(viewer_attributes, `${cloudfrontDomainName}${mediaUrl}`, token_policy);
+    var start = cloudfrontDomainName.length;
+    const myPath = playbackUrl.substring(start);
+
+    playbackUrl.startsWith(cloudfrontDomainName)
+    expect(playbackUrl.startsWith(cloudfrontDomainName)).toBeTruthy();
+    expect(playbackUrl.endsWith(mediaUrl)).toBeTruthy();
+    var cffEvent = {
+      "version": "1.0",
+      "viewer": {
+        "ip": myIp
+      },
+      "request": {
+        "method": "GET",
+        "uri": myPath,
+        "headers": {
+          "host": {
+            "value": "dklf7fsi4gpzd.cloudfront.net"
+          },
+          "user-agent": {
+            "value": myUserAgent
+          },
+          "referer": {
+            "value": myReferer
+          },
+          "origin": {
+            "value": "https://d26xf765ycwwd4.cloudfront.net"
+          }
+        }
+      }
+    };
+
+    const result = cff1.handler(cffEvent);
+    console.log(result);
+    expect(result.method).toBe("GET");
+
+  }, 70000);
+
+  test("Check token - valid token, ip=true", async () => {
+
+    const myIp = "54.240.197.233";
+    const myReferer =  'https://mycloudfrontdomainname.cloudfront.net';
+    const myUserAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:91.0) Gecko/20100101 Firefox/91.0';
+
+
+    var viewer_attributes = {
+      "ip": myIp,
+      "co": "FRANCE",
+      "reg": "ILE DE FRANCE",
+      "cty": "PARIS",
+      "headers": {
+        'cloudfront-viewer-address': myIp + ':31830',
+        'cloudfront-viewer-country': 'IE',
+        'content-length': '0',
+        'host': 'un25b5wnf5.execute-api.eu-west-1.amazonaws.com',
+        'referer': myReferer,
+        'user-agent': myUserAgent,
+        'via': '1.1 f9e2b62bbab7f16f69e97695da81e608.cloudfront.net (CloudFront)',
+      }
+    };
+
+    var token_policy =
+    {
+      "co": false,
+      "co_fallback": true,
+      "cty": false,
+      "cty_fallback": true,
+      "exc": [
+        "/ads/"
+      ],
+      "exp": "+3h",
+      "headers": [
+        "user-agent"
+      ],
+      "ip": true,
       "nbf": "1645000000",
       "paths": [
         "/out/v1/00c6ff982d404e2f940b48495b243b3c/"

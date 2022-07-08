f2c856924f526cfce0ba495c052ef05bde5b63cc
Croitoru <ccroito@bcd074351df1.ant.amazon.com>
2022-07-08 09:27:04 +0200

    increase test coverage

--- source/test/auto_revocation_workflow.test.ts
+++ source/test/auto_revocation_workflow.test.ts
@@ -16,13 +16,47 @@ test('Auto revocation session', () => {
     partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING }
   });
 
+  const myConfig = {
+    "main": {
+      "stack_name": "MYSTACK",
+      "wcu": 100,
+      "retention": 14,
+      "rotate_secrets_frequency": "m"
+    },
+    "api": {
+      "demo": false
+    },
+    "sessionRevocation": {
+      "trigger_workflow_frequency": 10,
+      "db_name": "default",
+      "table_name": "cloudfront_logs",
+      "request_ip_column": "requestip",
+      "ua_column_name": "useragent",
+      "referer_column_name": "referrer",
+      "uri_column_name": "uri",
+      "status_column_name": "status",
+      "response_bytes_column_name": "bytes",
+      "date_column_name": "date",
+      "time_column_name": "time",
+      "lookback_period": 10,
+      "ip_penalty": 1,
+      "ip_rate": 1,
+      "referer_penalty": 1,
+      "ua_penalty": 1,
+      "min_sessions_number": 3,
+      "min_session_duration": 30,
+      "score_threshold": 2.2,
+      "partitioned": 0
+    }
+  };
+
   new AutoRevokeSessionsWorkflow(
     stack,
     "GetSessions",
     {
       bucket: new s3.Bucket(stack, "SqlQuery"),
       dynamodbTable: myTable,
-      configuration: {} as IConfiguration
+      configuration: myConfig as IConfiguration
     }
   );
 

dd62a4df6e3bebe0d6752f5b09d50f4d14dc7513
Croitoru <ccroito@bcd074351df1.ant.amazon.com>
2022-07-20 16:40:58 +0200

    add licence msg

--- source/lambda/custom_resource_us_east_1/le.js
+++ source/lambda/custom_resource_us_east_1/le.js
@@ -1,4 +1,17 @@
-// Declare constants reqiured for the signature process
+/*********************************************************************************************************************
+ *  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.                                           *
+ *                                                                                                                    *
+ *  Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance    *
+ *  with the License. A copy of the License is located at                                                             *
+ *                                                                                                                    *
+ *      http://www.apache.org/licenses/LICENSE-2.0                                                                    *
+ *                                                                                                                    *
+ *  or in the 'license' file accompanying this file. This file is distributed on an 'AS IS' BASIS, WITHOUT WARRANTIES *
+ *  OR CONDITIONS OF ANY KIND, express or implied. See the License for the specific language governing permissions    *
+ *  and limitations under the License.                                                                                *
+ *********************************************************************************************************************/
+
+// Declare constants required for the signature process
 const crypto = require('crypto');
 const qs = require('querystring');
 const emptyHash = 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855';

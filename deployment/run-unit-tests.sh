8de0146343cf5bc3a6cf075cca6cccde06108964
Croitoru <ccroito@bcd074351df1.ant.amazon.com>
2022-07-21 15:01:01 +0200

    update test script

--- deployment/run-unit-tests.sh
+++ deployment/run-unit-tests.sh
@@ -15,6 +15,10 @@ cd ../source
 echo "Install node dependencies"
 npm install
 
+mv solution.context.json.template solution.context.json
+
+
+
 echo "Install NodeJs ws_secure_media_delivery layer dependencies for AWS Lambda"
 npm install --prefix resources/sdk/node/v1/
 
